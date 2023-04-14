5f570ba5b60394c5ea05351d7d1fac8588f322e2
Maicon Alves <AD648ZZ@mmm.com>
2023-04-13 22:02:54 -0300

    Test animate border

--- components/User/Register.vue
+++ components/User/Register.vue
@@ -6,20 +6,20 @@
                     <Card class="card-1" style="width: 30%; height: 70vh;">
                         <template #content>     
                             <div>
-                                <h2 class="flex flex-wrap align-items-center justify-content-center" style="color: black;">Registrar-se</h2>
+                                <h2 class="flex flex-wrap align-items-center justify-content-center" style="color: black;">Registre-se</h2>
                             </div>
                             <div class="flex flex-column align-items-center justify-content-center">
                                 <label class="mt-2" style="color:black;">Email</label>
-                                <InputText class="mt-2"/>
+                                <InputText v-model="userData.email" class="mt-2"/>
                                 <label class="mt-4" style="color: black;">Senha</label>
-                                <InputText class="mt-2"/>
+                                <InputText v-model="userData.password" class="mt-2"/>
                                 
                             </div>
                             <div class="flex flex-wrap align-items-center justify-content-center">
-                                <Button label="Registrar" class="mt-4" raised />
+                                <Button @click="login" label="Login" class="mt-4" raised />
                             </div>
                             <div class="flex flex-wrap align-items-center justify-content-center">
-                                <Button label="Já tem cadastro? clique aqui..." class="mt-4" text   />
+                                <Button label="Esqueceu a senha?" class="mt-4" text   />
                             </div>
                         </template>
                     </Card>
@@ -40,7 +40,17 @@
      export default {
         data() {
             return {
-
+                userData:{
+                    email:null,
+                    password:null
+                }
+            }
+        },
+        methods:{
+            login(){
+                if(this.userData.password == 'admin' || this.userData.password == '123'){
+                    window.location.replace('/dashboard')
+                }
             }
         }
     }
---
+++ pages/teste.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <div class="card center">
+            <h1 class="center">Card</h1>
+        </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+body {
+  height: 100vh;
+  font-family: sans-serif;
+  background-color: #EEE;
+  background-image: linear-gradient(45deg, #3cba5c, transparent);
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card {
+  position: relative;
+  overflow: hidden;
+  width: 237px;
+  height: 277px;
+  border-radius: 4px;
+}
+.card h1 {
+  border-radius: 4px;
+  width: 230px;
+  height: 270px;
+  background-image: linear-gradient(#ffffff, #ffffff);
+  color: #FFF;
+  animation: color 5s infinite linear;
+}
+.card h1::before {
+  content: '';
+  position: absolute;
+  width: 100px;
+  height: 400px;
+  background-image: linear-gradient(#05c438 , #05aa31);
+  animation: spin 2s infinite linear;
+  z-index: -1;
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+</style>
