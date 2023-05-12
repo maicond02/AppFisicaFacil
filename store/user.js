5b949cca47d273de581ca2f71120d126e870201b
maicond02 <eumaicondouglas.md45@gmail.com>
2023-05-12 19:48:34 -0300

    test em dev

--- store/user.js
+++ store/user.js
@@ -19,14 +19,14 @@ export const useUserStore = defineStore('USER', {
   
     actions: {
         async loadUsers(){            
-            let response = await fetch('http://localhost:3001/user', {    
-                headers: {
-                'Content-Type': 'application/json'
-                }, 
-                method: "GET", 
-            }).then(res => res.json())
-            this.getUserData = response
-            console.log(this.getUserData)
+            //let response = await fetch('http://localhost:3001/user', {    
+                //headers: {
+                //'Content-Type': 'application/json'
+               // }, 
+               // method: "GET", 
+            //}).then(res => res.json())
+            //this.getUserData = response
+            //console.log(this.getUserData)
         },
         async registerNewUser(data){            
             this.userData = data
